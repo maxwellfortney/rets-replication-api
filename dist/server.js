31bf54c950249ff8e08860de1fc25a4cce3c357d
Maxwell Fortney <maxwellfortney@gmail.com>
2021-09-19 23:20:53 -0400

    Attempt to fix gitignore issue

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-import app from "./config/app";
-import env from "./environment";
-const PORT = env.getPort();
-app.listen(PORT, () => {
-    console.log("Express server listening on port " + PORT);
-});
-//# sourceMappingURL=server.js.map
